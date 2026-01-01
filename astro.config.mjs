fa55d961507970ecc10cb97bd11342a2de5d0322
Xetha666 <bryans27092000@gmail.com>
2025-12-31 21:44:05 -0500

    refactor: update Astro configuration to use Vite with Tailwind CSS and set UTF-8 charset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [ tailwindcss() ],
+  },
+  
   output: 'static',
 });
